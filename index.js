160a9361b4b933cbafe0f1aff2a819c846dfcd86
Nishan0920 <nishanbista336@gmail.com>
2026-08-31 16:14:55 +0545

    down

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ import path from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
 
-import MongoDB from "./db.js";
+import MongoDB from "../db.js";
 
-import entry from "./Routes/Entry.js";
-import customerData from "./Routes/Customers.js";
-import inventoryData from "./Routes/Inventory.js";
-import prescription from "./Routes/Prescriptions.js";
-import staff from "./Routes/Staff.js";
-import salary from "./Routes/Salary.js";
-import RentAndLease from "./Routes/RentAndLease.js";
-import expenses from "./Routes/Expenses.js";
-import sales from "./Routes/Sales.js";
+import entry from "../Routes/Entry.js";
+import customerData from "../Routes/Customers.js";
+import inventoryData from "../Routes/Inventory.js";
+import prescription from "../Routes/Prescriptions.js";
+import staff from "../Routes/Staff.js";
+import salary from "../Routes/Salary.js";
+import RentAndLease from "../Routes/RentAndLease.js";
+import expenses from "../Routes/Expenses.js";
+import sales from "../Routes/Sales.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -48,7 +48,7 @@ app.use(async (req, res, next) => {
 
 app.use(
   "/uploads",
-  express.static(path.join(__dirname, "uploads"))
+  express.static(path.join(__dirname, "../uploads"))
 );
 
 app.use("/api", entry);
